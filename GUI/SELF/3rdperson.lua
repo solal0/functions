85312cfcad9bb0933b73cd959750c15b982c8123
Skira <100592118+solal0@users.noreply.github.com>
2026-01-15 17:23:35 +0100

    Update 3rdperson.lua

--- GUI/SELF/3rdperson.lua
+++ GUI/SELF/3rdperson.lua
@@ -1,7 +1,12 @@
-return function(state)
-    if state then
-        print("Third person enabled")
-    else
-        print("Third person disabled")
+return function(enabled)
+    hook.Remove("CalcView", "ThirdPersonCam")
+    if enabled then
+        hook.Add("CalcView", "ThirdPersonCam", function(ply, pos, angles, fov)
+            local view = {}
+            view.origin = pos - angles:Forward() * 100 + Vector(0,0,10)
+            view.angles = angles
+            view.fov = fov
+            return view
+        end)
     end
 end
