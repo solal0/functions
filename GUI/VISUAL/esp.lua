09283d4da70fce9fce1a25e3127f85c547c5ada0
Skira <100592118+solal0@users.noreply.github.com>
2026-01-15 17:24:02 +0100

    Update esp.lua

--- GUI/VISUAL/esp.lua
+++ GUI/VISUAL/esp.lua
@@ -1,3 +1,21 @@
 return function(option)
-    print("ESP option set to:", option)
+    hook.Remove("HUDPaint", "ESP_HUD")
+    if option == "OFF" then return end
+
+    hook.Add("HUDPaint", "ESP_HUD", function()
+        for _, ply in ipairs(player.GetAll()) do
+            if ply == LocalPlayer() then continue end
+            if option == "Players" or option == "All" then
+                local pos = ply:EyePos():ToScreen()
+                draw.SimpleText(ply:Nick(), "DermaDefault", pos.x, pos.y, Color(255,0,0), TEXT_ALIGN_CENTER)
+            end
+        end
+        if option == "Props" or option == "All" then
+            for _, ent in ipairs(ents.GetAll()) do
+                if not ent:IsValid() or ent:IsPlayer() then continue end
+                local pos = ent:GetPos():ToScreen()
+                draw.SimpleText("Prop", "DermaDefault", pos.x, pos.y, Color(0,255,0), TEXT_ALIGN_CENTER)
+            end
+        end
+    end)
 end
